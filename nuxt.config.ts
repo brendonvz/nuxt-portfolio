744562f70c8a07a9fbd844a94c1d05e0f901d4bf
brendonvz <32501372+brendonvz@users.noreply.github.com>
2024-04-19 10:43:02 -0400

    adding base of portfolio

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,27 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
 	components: [
-			{
-				path: '~/components',
-				pathPrefix: false,
-			},
-		],
-  modules: ['@nuxtjs/tailwindcss', 'nuxt-icon'],
+		{
+			path: '~/components',
+			pathPrefix: false,
+		},
+	],
+	runtimeConfig: {
+		githubToken: process.env.GITHUB_TOKEN
+	},
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', '@nuxt/content', '@nuxtjs/apollo'],
+	content: {
+    highlight: {
+      theme: 'nord',
+      preload: ['ts','js','css','java','json','bash','vue']
+    }
+  },
+  apollo: {
+    clients: {
+      default: {
+        tokenName: 'github-token',
+        httpEndpoint: 'https://api.github.com/graphql'
+      }
+    }
+  }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,10 @@
 <script setup>
+const { data: posts } = await useAsyncData('latest-posts', () =>
+  queryContent('/blog')
+    .sort({ data: 1 })
+    .limit(3)
+    .find()
+)
 </script>
 
 <template>
@@ -27,10 +33,10 @@
     </div>
     <img src="~/assets/images/profilepic.png" class="w-1/2 md:max-w-sm p-8 mx-auto" />
   </section>
-	<section>
+  <section>
     <h2 class="text-3xl font-bold mt-8">Latest Blog Posts</h2>
-    <!-- <div class="grid md:grid-cols-3 pt-8 gap-10">
+    <div class="grid md:grid-cols-3 pt-8 gap-10">
       <Post :posts="posts" />
-    </div> -->
+    </div>
   </section>
 </template>
