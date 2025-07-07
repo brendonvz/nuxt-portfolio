c8c4dd99cd28d3039ff2ba00c95ea81561f7f4ff
brendonvz <32501372+brendonvz@users.noreply.github.com>
2025-07-07 16:23:08 -0400

    force new favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -59,9 +59,9 @@ export default defineNuxtConfig({
 			title: "Brendon van Zanten - Full Stack Web Developer",
 			titleTemplate: "%s",
 			link: [
-				{ rel: "icon", type: "image/svg+xml", href: "/favicon/favicon.svg" },
-				{ rel: "icon", type: "image/png", href: "/favicon/favicon-96x96.png", sizes: "96x96" },
-				{ rel: "shortcut icon", href: "/favicon/favicon.ico" },
+				{ rel: "icon", type: "image/svg+xml", href: "/favicon/favicon.svg?v=2" },
+				{ rel: "icon", type: "image/png", href: "/favicon/favicon-96x96.png?v=2", sizes: "96x96" },
+				{ rel: "shortcut icon", href: "/favicon/favicon.ico?v=2" },
 				{ rel: "apple-touch-icon", type: "image/svg+xml", href: "/favicon/apple-touch-icon.svg" },
 				{ rel: "apple-touch-icon", sizes: "180x180", href: "/favicon/apple-touch-icon.png" },
 				{ rel: "manifest", href: "/favicon/site.webmanifest" },
