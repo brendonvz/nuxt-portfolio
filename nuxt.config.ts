772975376e56d158ba06504a9d0b72d7b67e5045
brendonvz <32501372+brendonvz@users.noreply.github.com>
2025-07-07 16:42:18 -0400

    change favicon order

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -59,11 +59,13 @@ export default defineNuxtConfig({
 			title: "Brendon van Zanten - Full Stack Web Developer",
 			titleTemplate: "%s",
 			link: [
-				{ rel: "icon", type: "image/svg+xml", href: "/favicon/favicon.svg?v=2" },
-				{ rel: "icon", type: "image/png", href: "/favicon/favicon-96x96.png?v=2", sizes: "96x96" },
-				{ rel: "shortcut icon", href: "/favicon/favicon.ico?v=2" },
-				{ rel: "apple-touch-icon", type: "image/svg+xml", href: "/favicon/apple-touch-icon.svg" },
+				{ rel: "icon", href: "/favicon/favicon.ico?v=3" },
+				{ rel: "shortcut icon", href: "/favicon/favicon.ico?v=3" },
+				{ rel: "icon", type: "image/svg+xml", href: "/favicon/favicon.svg?v=3" },
+				{ rel: "icon", type: "image/png", href: "/favicon/favicon-96x96.png?v=3", sizes: "96x96" },
+				{ rel: "icon", type: "image/png", href: "/favicon/favicon-96x96.png?v=3", sizes: "32x32" },
 				{ rel: "apple-touch-icon", sizes: "180x180", href: "/favicon/apple-touch-icon.png" },
+				{ rel: "apple-touch-icon", type: "image/svg+xml", href: "/favicon/apple-touch-icon.svg" },
 				{ rel: "manifest", href: "/favicon/site.webmanifest" },
 			],
 			meta: [
